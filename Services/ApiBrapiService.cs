949aaa70b387faf7e5c6f6ba87eb175f3b14821f
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-09-05 18:19:03 -0300

    project

--- Services/ApiBrapiService.cs
+++ Services/ApiBrapiService.cs
@@ -1,24 +1,36 @@
 ﻿using Newtonsoft.Json;
 using stockz_bucketz_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace stockz_bucketz_api.Services
 {
     public class ApiBrapiService
     {
-        private HttpClient _httpClient;
+        private readonly HttpClient _httpClient;
+        private readonly string _token;
 
         public ApiBrapiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            _token = Environment.GetEnvironmentVariable("tokenBrapi", EnvironmentVariableTarget.Machine);
         }
 
         public async Task<List<StockBrapi>> Get()
         {
-            var response = await _httpClient.GetAsync("https://brapi.dev/api/quote/list?token=e1N2Rv5dStx9u6xCzsveoN?search=ibo");
+            var url = $"https://brapi.dev/api/quote/list?token={_token}?search=ibo";
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to retrieve data from BrAPI.");
+            }
 
             var data = await response.Content.ReadAsStringAsync();
-            var reponseBrapiObj = JsonConvert.DeserializeObject<ResponseBrapi>(data);
-            var stocks = reponseBrapiObj.Stocks;
+            var responseBrapiObj = JsonConvert.DeserializeObject<ResponseBrapi>(data);
+            var stocks = responseBrapiObj?.Stocks ?? new List<StockBrapi>();
             return stocks;
         }
     }
